0197bd103a5e3a8abbf6df4931f90b50bce2117c
PRbrate <pauloteles08@hotmail.com>
2026-04-29 13:11:20 -0300

    Feat: select data to return and new Model UserData

---
+++ TaskUnMEP/Models/UserData.cs
@@ -0,0 +1,16 @@
+﻿namespace TaskUnMEP.Models
+{
+    public class UserData
+    {
+        public UserData(int userId, string userName, int totalMinutes)
+        {
+            this.userId = userId;
+            this.userName = userName;
+            this.totalMinutes = totalMinutes;
+        }
+
+        public int userId { get; set; }
+        public string userName { get; set; }
+        public int totalMinutes { get; set; }
+    }
+}
--- TaskUnMEP/Program.cs
+++ TaskUnMEP/Program.cs
@@ -25,8 +25,48 @@ var usersValid = userTasks.Where(u => u.minutes >= 0).ToList();
 var totalMinutes = usersValid.Select(u => u.minutes).Sum();
 var taskByGroup  = usersValid.GroupBy(u => u.taskId).ToList();
 
+var taskToReturn = new List<TaskData>();
 
+foreach (var group in taskByGroup)
+{
 
+    var totalByTask = group.Sum(v => v.minutes);
+    int id = group.Select(u => u.taskId).First();
+    string name = group.Select(u => u.taskName).First();
+    int sum = group.Select(u => u.minutes).Sum();
+
+    double percentage = (double)totalByTask / totalMinutes * 100;
+
+    string percentageReturn = $"{percentage:F2}%";
+
+    var task = new TaskData(id, name, sum, percentageReturn);
+    taskToReturn.Add(task);
+}
+
+var moreWork = taskByGroup.MaxBy(t => t.Select(u => u.minutes).Sum());
+
+var top3TasksPercentage = taskToReturn
+    .OrderByDescending(t => t.totalMinutes)
+    .Take(3)
+    .ToList();
+
+var employee = usersValid.GroupBy(u => u.userId).ToList();
+var employeeReturn = new List<UserData>();
+
+foreach(var emp in employee)
+{
+    var id = emp.Select(u => u.userId).First();
+    var name = emp.Select(u => u.userName).First();
+    int totalEmpMinutes = emp.Select(u => u.minutes).Sum();
+    employeeReturn.Add(new UserData(id, name, totalEmpMinutes));
+}
+
+var top3Employees = employeeReturn
+    .OrderByDescending(t => t.totalMinutes)
+    .Take(3)
+    .ToList();
+
+Console.ReadKey();
 
 
 
