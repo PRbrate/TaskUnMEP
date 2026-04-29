ebf87c746dcaec9b06a2a6661d8d0309fae22129
PRbrate <pauloteles08@hotmail.com>
2026-04-29 11:26:33 -0300

    Feat: Read data

--- TaskUnMEP/Program.cs
+++ TaskUnMEP/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿using Newtonsoft.Json;
+using TaskUnMEP.Models;
+
+List<UserTask> userTasks = new();
+string path = "../../../data.json";
+
+try
+{
+    using (StreamReader r = new StreamReader(path))
+    {
+
+        string json = r.ReadToEnd();
+        userTasks = JsonConvert.DeserializeObject<List<UserTask>>(json);
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine("Aquivo não encontrado: " + e.Message);
+}
+
+
+
+
